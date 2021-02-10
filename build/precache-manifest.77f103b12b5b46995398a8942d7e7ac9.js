30bd7d5c99ddeccda272248810de471b627b9cf3
lakshitmadaan20 <51285500+lakshitmadaan20@users.noreply.github.com>
2021-02-10 13:29:01 +0530

    Build added

---
+++ build/precache-manifest.77f103b12b5b46995398a8942d7e7ac9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc1a45eab44e85a5abe735ed3d1e131c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc9087d067f119743618",
+    "url": "/static/css/main.f35160a8.chunk.css"
+  },
+  {
+    "revision": "411ee1f7566ecb9affaf",
+    "url": "/static/js/2.d4996fd2.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.d4996fd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc9087d067f119743618",
+    "url": "/static/js/main.3348c772.chunk.js"
+  },
+  {
+    "revision": "e173dfe45a061c6e2715",
+    "url": "/static/js/runtime-main.20a4ba51.js"
+  }
+]);
