343918f0e5d47d29616db7db8555e18f4a97b938
lakshitmadaan20 <51285500+lakshitmadaan20@users.noreply.github.com>
2021-02-10 12:50:13 +0530

    Updates

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,10 @@
-import React, { Fragment, Component } from 'react'
-import { Typography } from '@material-ui/core'
-import {Button} from 'semantic-ui-react'
-
+import React, { Fragment } from 'react'
 
 export default function About() {
     return (
-           <div className="row">
+           
+        <Fragment>
+            <div className="row">
                <div className="col-6">
                    <h2 className="text-center">What Is Covid-19?</h2>
                    <div style={{marginLeft:"6px"}} className="jumbotron">
@@ -14,10 +13,7 @@ export default function About() {
                     </p>
                     <p style={{fontSize:"16px"}}>
                      Coronaviruses are zoonotic, meaning they are transmitted between animals and people. Detailed investigations found that SARS-CoV was transmitted from civet cats to humans and MERS-CoV from dromedary camels to humans. Several known coronaviruses are circulating in animals that have not yet infected humans.
-                    </p>
-                    <p style={{fontSize:'16px'}}>
-                     Common signs of infection include respiratory symptoms, fever, cough, shortness of breath and breathing difficulties. In more severe cases, infection can cause pneumonia, severe acute respiratory syndrome, kidney failure and even death.
-                    </p>   
+                    </p> 
                    </div> 
                 </div>
                <div className="col-6">
@@ -25,9 +21,10 @@ export default function About() {
                   <img
                    alt="Covid-19"
                    src="https://www.lapuste.com/sites/default/files/inline-images/bcpha_poster.png"
-                   style={{display:'block',width:'60%',marginLeft:'auto', marginTop:'auto', marginRight:'auto', marginBottom:'auto'}}
+                   style={{display:'block',width:'50%',marginLeft:'auto', marginTop:'auto', marginRight:'auto', marginBottom:'auto'}}
                   />
                </div>
            </div>
+        </Fragment>
     )
 }
